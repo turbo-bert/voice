5f8c7f2c9f432d3947f812736105b480d835ffdd
Robert Degen <robert.degen@mailbox.org>
2024-03-15 00:31:26 +0100

    ...

---
+++ src/sequences-de/test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+## ./zzz_countcount.sh
+
+
+# for macos incl. ffmpeg
+
+#rm -f *.aiff *.txt *.mp3
+
+p() {
+    L=de
+    WHAT="$1"
+    WHATF=$L-$(echo -n "$WHAT" | tr ' ' '-').txt.mp3
+    echo "$WHATF"
+    afplay $WHATF
+}
+
+
+
+pfull() {
+    SV=0.4
+    SPAUSE=0.5
+    S="$1"
+    SL=${#S}
+    ((SL=SL-1))
+    for I in $(seq 0 $SL); do
+        C=${S:I:1}
+        if [[ "$C" = " " ]]; then
+            sleep $SPAUSE
+        else
+            p "$C" &
+            sleep $SV
+        fi
+    done
+}
+
+
+
+pfull "1234567890abcdef"
