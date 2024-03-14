f2a752377e2d9c21e9773441401dd7586b0c9ba0
Robert Degen <robert.degen@mailbox.org>
2024-03-14 20:55:35 +0100

    ...

--- src/sequences/run.sh
+++ src/sequences/run.sh
@@ -6,7 +6,7 @@
 
 # for macos incl. ffmpeg
 
-rm -f *.aiff *.txt *.mp3
+#rm -f *.aiff *.txt *.mp3
 
 fromtext() {
     VOICE=$1
@@ -14,14 +14,17 @@ fromtext() {
     WHAT="$3"
     WHATF=$L-$(echo -n "$WHAT" | tr ' ' '-').txt
     echo -n "$WHAT" > $WHATF
-    #tess $WHATF
-    say -v $VOICE -f "$WHATF" -o "$WHATF.aiff"
-    ffmpeg -i "$WHATF.aiff" -vn -acodec mp3 -ab 64k -ar 22050 -y "$WHATF.mp3"
-    rm -f "$WHATF.aiff"
+    if [[ ! -f $WHATF.mp3 ]]; then
+        #tess $WHATF
+        say -v $VOICE -f "$WHATF" -o "$WHATF.aiff"
+        ffmpeg -i "$WHATF.aiff" -vn -acodec mp3 -ab 64k -ar 22050 -y "$WHATF.mp3"
+        rm -f "$WHATF.aiff"
+    fi
 }
 
 fromtext tess en "started and ready"
 fromtext tess en "downloading"
 fromtext tess en "please wait"
+fromtext tess en "one moment"
 
 . inc_countdown.sh
